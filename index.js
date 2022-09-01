091510e3125c114f23c12c89f9df1fd8603e0cdd
Kaden <kadengraybill@gmail.com>
2022-09-01 15:48:47 -0500

    commit tree a60d97f6fc471a478b399bee06b949fc9cbf02ae

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+    //...
+
+
+//1st answer
+ /* 
+function superbowlWin('W'){
+    return record.year
+}
+
+const winYear = record.map(superbowlWin)
+*/
+/*
+//2nd answer
+let superbowlWin = record.map(function(blah){
+   if(blah.result === 'W'){
+     return blah.result;
+   } else {
+    return undefined;
+   }
+})
+*/
+
+//3rd answer
+function superbowlWin(record){
+    const winYear = record.find(record => record.result === 'W')
+    return winYear ? winYear.year : undefined;
+}
+superbowlWin(record)
